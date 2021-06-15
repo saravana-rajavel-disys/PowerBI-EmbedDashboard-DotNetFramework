5af9f55df69a408e604aa3786bda0d12f1c81626
Saravanakumar Rajavel <saravana.rajavel@disys.com>
2021-06-15 21:09:04 +0530

    Add project files.

---
+++ AppOwnsData/Services/EmbedService.cs
@@ -0,0 +1,70 @@
+﻿// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+// ----------------------------------------------------------------------------
+
+namespace AppOwnsData.Services
+{
+    using AppOwnsData.Models;
+    using Microsoft.PowerBI.Api;
+    using Microsoft.PowerBI.Api.Models;
+    using Microsoft.Rest;
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+    using System.Threading.Tasks;
+
+    public static class EmbedService
+    {
+        private static readonly string urlPowerBiServiceApiRoot = ConfigurationManager.AppSettings["urlPowerBiServiceApiRoot"];
+
+        public static async Task<PowerBIClient> GetPowerBiClient()
+        {
+            var tokenCredentials = new TokenCredentials(await AadService.GetAccessToken(), "Bearer");
+            return new PowerBIClient(new Uri(urlPowerBiServiceApiRoot), tokenCredentials);
+        }
+
+        /// <summary>
+        /// Get embed params for a dashboard
+        /// </summary>
+        /// <returns>Wrapper object containing Embed token, Embed URL for single dashboard</returns>
+        public static async Task<DashboardEmbedConfig> EmbedDashboard(Guid workspaceId)
+        {
+            // Create a Power BI Client object. It will be used to call Power BI APIs.
+            using (var client = await GetPowerBiClient())
+            {
+                // Get a list of dashboards.
+                var dashboards = await client.Dashboards.GetDashboardsInGroupAsync(workspaceId);
+
+                // Get the first report in the workspace.
+                var dashboard = dashboards.Value.FirstOrDefault();
+
+                if (dashboard == null)
+                {
+                    throw new NullReferenceException("Workspace has no dashboards");
+                }
+
+                // Generate Embed Token.
+                var generateTokenRequestParameters = new GenerateTokenRequest(accessLevel: "view");
+                var tokenResponse = await client.Dashboards.GenerateTokenInGroupAsync(workspaceId, dashboard.Id, generateTokenRequestParameters);
+
+                if (tokenResponse == null)
+                {
+                    throw new NullReferenceException("Failed to generate embed token");
+                }
+
+                // Generate Embed Configuration.
+                var dashboardEmbedConfig = new DashboardEmbedConfig
+                {
+                    EmbedToken = tokenResponse,
+                    EmbedUrl = dashboard.EmbedUrl,
+                    DashboardId = dashboard.Id
+                };
+
+                return dashboardEmbedConfig;
+            }
+        }
+    }
+}
